e0f4a5eb8bf71085b333152f417b6084d39edee9
FRANCK KASONGO <109921642+Franckkasongo15@users.noreply.github.com>
2025-01-15 13:50:13 +0200

    Initialisation projet et installation css

---
+++ views/index.view.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="/public/assets/style.css">
+    <title>Document</title>
+</head>
+<body >
+    <header>
+        <nav class="">
+            <ul>
+                <li><a href="#">KIZZ</a></li>
+                <li><a href="#">Home</a></li>
+                <li><a href="#">Training</a></li>
+                <li><a href="#">Ranking</a></li>
+            </ul>
+        </nav>
+    </header>
+</body>
+</html>
